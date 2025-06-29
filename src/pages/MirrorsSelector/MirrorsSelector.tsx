47def9f73d470387361e4faa7f799934a3d3b408
prototype <3489849684@qq.com>
2025-06-30 07:57:18 +0800

    微调选择镜像页面

--- src/pages/MirrorsSelector/MirrorsSelector.tsx
+++ src/pages/MirrorsSelector/MirrorsSelector.tsx
@@ -41,7 +41,7 @@ export default function MirrorsSelector(){
   useEffect(() => {
     const RTTS: number[] = urlRTTs.map((urlRTT) => {return Number(urlRTT.RTT.replace("ms", ""))}).filter((RTT) => {return RTT != 0})
     RTTS.sort()
-    if (urlRTTs.filter((urlRTT) => {return Number(urlRTT.RTT.replace("ms", "")) == RTTS[0]})[0] != undefined){
+    if (urlRTTs.filter((urlRTT) => {return Number(urlRTT.RTT.replace("ms", "")) == RTTS[0]})[0] != undefined && RTTS.length == availableMirrors.length){
       const fastestMirrorURL = urlRTTs.filter((urlRTT) => {return Number(urlRTT.RTT.replace("ms", "")) == RTTS[0]})[0].url
       const fastestMirrorIDTemp = availableMirrors.filter((availableMirror) => {return availableMirror.url == fastestMirrorURL})[0].id
       setFastestMirrorID(fastestMirrorIDTemp)
