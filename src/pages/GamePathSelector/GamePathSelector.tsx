94636d1d4b0c818cf414a07ebf22e66ca9a73f68
prototype <3489849684@qq.com>
2025-06-30 18:36:13 +0800

    更新对话框选择游戏路径功能

--- src/pages/GamePathSelector/GamePathSelector.tsx
+++ src/pages/GamePathSelector/GamePathSelector.tsx
@@ -4,6 +4,7 @@ import { useNextButton } from "../../hooks/useNextButton";
 import { usePage } from "../../hooks/usePage";
 import { useLocalGameInfo } from "../../hooks/useLocalGameInfo";
 import { InstallationInfoKey, useInstallationInfo } from "../../hooks/useInstallationInfo";
+import { open } from "@tauri-apps/plugin-dialog";
 
 export default function GamePathSelector(){
   const [path, setPath] = useState("")
@@ -39,7 +40,14 @@ export default function GamePathSelector(){
       <PageHeader title="设置游戏路径" description="将游戏路径填入下方输入框中，该路径必须包含DELTARUNE.EXE，您可以在资源管理器中手动查找，或使用STEAM进行查找"/>
       <div className="option">
         <label className="name">游戏路径</label>
-        <input className="input" onInput={(e) => {setPath(e.currentTarget.value)}}/>
+        <div className="input-group">
+          <input className="input" onInput={(e) => {setPath(e.currentTarget.value)}} value={path}/>
+          <button className="selector-button" onClick={() => {
+            open({directory: true}).then((selectedPath) => {
+              setPath(selectedPath ?? "")
+            })
+          }}>...</button>
+        </div>
       </div>
     </div>
   )
